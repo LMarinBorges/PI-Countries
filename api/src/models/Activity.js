5d350b6ea9a0157a1b7b524f971ac23920bd4067
Liam Marin <liam.marin03926@protonmail.com>
2023-02-28 19:17:27 -0300

    feat: define Activity model

---
+++ api/src/models/Activity.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Activity", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+      type: DataTypes.SMALLINT,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    duration: {
+      type: DataTypes.SMALLINT,
+    },
+    season: {
+      type: DataTypes.SMALLINT,
+      validate: {
+        min: 0,
+        max: 3,
+      },
+    },
+  });
+};
