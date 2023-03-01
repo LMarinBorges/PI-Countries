2e39fd8b571a5f66d37247e3e9e76ee8046ee2a2
Liam Marin <liam.marin03926@protonmail.com>
2023-03-01 15:38:58 -0300

    feat: implement api routes

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -1,5 +1,9 @@
 const { Router } = require("express");
+const activityController = require("../controllers/activityController.js");
 
 const router = Router();
+
+router.get("/", activityController.listActivities);
+router.get("/", activityController.createActivity);
 
 module.exports = router;
--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,5 +1,10 @@
 const { Router } = require("express");
+const countryController = require("../controllers/countryController.js");
 
 const router = Router();
+
+router.get("/", countryController.listCountries);
+router.get("/name", countryController.findCountries);
+router.get("/:countryId", countryController.getCountry);
 
 module.exports = router;
