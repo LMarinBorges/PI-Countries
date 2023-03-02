572471a85906e9dfd3ebb447eb07196e57b086e1
Liam Marin <liam.marin03926@protonmail.com>
2023-03-01 22:39:11 -0300

    refactor: simplify db.js

--- api/src/db.js
+++ api/src/db.js
@@ -6,28 +6,21 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
+  { logging: false, native: false }
 );
 
-// Leemos todos los archivos de la carpeta Models,
-// los requerimos e injectamos la conexion (sequelize) a todos los modelos.
+// Requerimos todos los modelos e injectamos la conneción a cada uno.
 fs.readdirSync(path.join(__dirname, "models"))
   .filter((file) => file.endsWith(".js"))
   .map((file) => require(path.join(__dirname, "models", file)))
   .forEach((model) => model(sequelize));
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
+// Definimos relaciones.
 const { Activity, Country } = sequelize.models;
-
-// Relaciones
 Activity.belongsToMany(Country, { through: "country_activities" });
 Country.belongsToMany(Activity, { through: "country_activities" });
 
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Country, Activity } = require('./db.js');
-  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+  ...sequelize.models, // Modelos
+  sequelize, // Instancia de sequelize
 };
