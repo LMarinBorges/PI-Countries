c67731d1eadf41611a106657df4f8190be2b6f64
Liam Marin <liam.marin03926@protonmail.com>
2023-03-02 00:33:58 -0300

    feat: implement country controller

--- api/src/controllers/countryController.js
+++ api/src/controllers/countryController.js
@@ -1,13 +1,28 @@
+const { Op } = require("sequelize");
 const { Country } = require("../db.js");
+const asyncHandler = require("../helpers/asyncHandler.js");
+const { notFound } = require("../helpers/response.js");
 
-exports.listCountries = (req, res) => {
-  res.send("UNIMPLEMENTED: GET /countries");
-};
+exports.listCountries = asyncHandler(async (req, res) => {
+  const countries = await Country.findAll();
+  res.json(countries);
+});
 
-exports.findCountries = (req, res) => {
-  res.send("UNIMPLEMENTED: GET /countries/name?q=...");
-};
+exports.findCountries = asyncHandler(async (req, res) => {
+  const { q } = req.query;
+  const countries = await Country.findAll({
+    where: { name: { [Op.iLike]: `%${q}%` } },
+  });
+  if (countries.length === 0)
+    return notFound(res, "No countries found that match query.");
+  return res.json(countries);
+});
 
-exports.getCountry = (req, res) => {
-  res.send("UNIMPLEMENTED: GET /countries/:countryId");
-};
+exports.getCountry = asyncHandler(async (req, res) => {
+  const { countryId } = req.params;
+  const country = await Country.findByPk(countryId, {
+    include: "activities",
+  });
+  if (country === null) return notFound(res, "Country not found.");
+  res.json(country);
+});
