d7d937421636bebae6aaf3e37a9cc725f867fbfa
Liam Marin <liam.marin03926@protonmail.com>
2023-03-03 18:58:35 -0300

    feat: add more response helpers

--- api/src/helpers/response.js
+++ api/src/helpers/response.js
@@ -5,3 +5,19 @@ exports.notFound = (res, message) => {
     message,
   });
 };
+
+exports.badRequest = (res, errors) => {
+  res.status(400).json({
+    code: "bad_request",
+    status: 400,
+    errors,
+  });
+};
+
+exports.created = (res, payload) => {
+  res.status(201).json({
+    code: "created",
+    status: 201,
+    payload,
+  });
+};
